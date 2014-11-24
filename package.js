5241026e8e6c287a32e1add28676afc643a72679
Joe Danziger <joe@ticc.net>
2014-11-24 02:55:55 -0500

    update versions and add testing code

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'danziger:transloadit',
   summary: 'Transloadit API Wrapper for Meteor',
-  version: '0.1.1',
+  version: '0.1.2',
   git: 'https://github.com/transloadit/meteor-sdk.git'
 });
 
@@ -10,4 +10,9 @@ Npm.depends({ "transloadit": "1.2.1" });
 Package.onUse(function(api) {
   if (api.export) api.export('TransloaditClient', 'server');
   api.addFiles('transloadit_npm.js', 'server');
+});
+
+Package.onTest(function(api) {
+  api.use(["danziger:transloadit", "tinytest", "test-helpers"]);
+  api.addFiles('transloadit_tests.js', 'server');
 });
